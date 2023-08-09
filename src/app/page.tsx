aa00d7e502379dc97c92d0c9912274d8a9611f12
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-08 23:22:49 -0300

    refactor: removed CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-'use client'
-
-import { styled } from "styled-components"
-
-
-const PageContainer = styled.div`
-	color: white;
-`
-
 export default function Home() {
 	return (
-		<PageContainer>
+		<div>
 			Patrick Gunnar
-		</PageContainer>
+		</div>
 	)
 }
 
