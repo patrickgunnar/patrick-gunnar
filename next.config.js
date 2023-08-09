d1b81b170826053251ed62728c14f9afc6158864
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-09 14:07:24 -0300

    feat: enabled styled-components compiler

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    // Enable styled-components via compiler options
+    compiler: {
+        styledComponents: true,
+    },
+}
 
 module.exports = nextConfig
