25c7bb5cf8578b821fc9c86b63814b679d6af32f
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-09 13:40:18 -0300

    feat: added navbar options

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -5,20 +5,83 @@ const NavbarContainer = styled.nav`
     background: linear-gradient(to top, var(--background-007) 0%, var(--background-008) 100%);
     filter: drop-shadow(0 0 1px var(--background-006));
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(0, calc(92% / 7)));
 
     position: relative;
 
     height: 9%;
     width: 79%;
+
+    > :last-child {
+        padding-right: 75%;
+        width: 160%;
+    }
+`
+
+const NavbarOpt = styled.button`
+    background: transparent;
+    border: none;
+
+    cursor: pointer;
+    color: inherit;
+    font-weight: 400;
+    font-family: inherit;
+    font-size: .85rem;
+
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    &:hover {
+        background: linear-gradient(
+            109.6deg, var(--background-015) 11.2%, var(--background-014) 51.2%, var(--background-005) 98.6%
+        );
+    }
+
+    &::after {
+        content: " ";
+
+        background: linear-gradient(to bottom, var(--background-009) 0%, var(--background-013) 100%), 
+                    radial-gradient(at top center, var(--background-010) 0%, var(--background-010) 120%) 
+                    var(--background-011);
+        border: none;
+        border-radius: 100%;
+
+        position: absolute;
+        top: 50%;
+        left: 100%;
+        transform: translate(-50%, -50%);
+        
+        height: 50%;
+        width: 1px;
+    }
 `
 
 const Navbar = () => {
     return (
         <NavbarContainer>
-            Navbar
+            <NavbarOpt>
+                Home
+            </NavbarOpt>
+            <NavbarOpt>
+                Myself
+            </NavbarOpt>
+            <NavbarOpt>
+                Skills
+            </NavbarOpt>
+            <NavbarOpt>
+                Projects
+            </NavbarOpt>
+            <NavbarOpt>
+                Resumé
+            </NavbarOpt>
+            <NavbarOpt>
+                Contact
+            </NavbarOpt>
+            <NavbarOpt>
+                Articles
+            </NavbarOpt>
         </NavbarContainer>
     );
 }
