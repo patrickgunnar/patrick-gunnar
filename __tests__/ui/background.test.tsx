4d318bd432185e62790f9059ded37258388c9d3b
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-17 19:03:01 -0300

    test: background canvas template

--- __tests__/ui/background.test.tsx
+++ __tests__/ui/background.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import CanvasTemplate from "@/components/background/canvas-template";
 
+
 test('background matrix', async () => {
     render(<CanvasTemplate />)
 
