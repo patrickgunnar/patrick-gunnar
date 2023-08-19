3174e0dd673abda2cd17fea8802ced8bd332f1ab
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-19 08:32:39 -0300

    feat: content wrapper

---
+++ src/components/content/content-wrapper.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { laptop } from "@/app/screens";
+import { styled } from "styled-components";
+
+
+interface ContentWrapperProps {
+    children: React.ReactNode
+}
+
+const ContentContainer = styled.div`
+    box-sizing: border-box;
+
+    display: flex;
+    gap: 10px;
+    flex-direction: column;
+    justify-content: start;
+    align-items: center;
+
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    overflow: hidden;
+    overflow-y: auto;
+
+    @media only screen and (min-width: ${laptop}) {
+        flex-direction: row;
+        justify-content: start;
+        align-items: start;
+    }
+`
+
+const ContentWrapper: React.FC<ContentWrapperProps> = ({ children }) => {
+    return (
+        <ContentContainer>
+            {children}
+        </ContentContainer>
+    );
+}
+ 
+export default ContentWrapper;
