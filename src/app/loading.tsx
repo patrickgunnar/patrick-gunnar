1faf1e7ad9821e95b4069a649b61ece52c614931
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-21 12:50:41 -0300

    feat: loading content

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import LoadingWave from "@/components/loading/loading-wave";
+
+
+const Loading = () => {
+    return (
+        <LoadingWave />
+    );
+}
+ 
+export default Loading;
