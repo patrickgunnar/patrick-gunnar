6d1f44923c1e68904a0493f36d37d818c767de1f
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-12 13:39:50 -0300

    refactor: improved styles

--- src/app/projects/components/ProjectsContent.tsx
+++ src/app/projects/components/ProjectsContent.tsx
@@ -1,8 +1,8 @@
-'use client'
+"use client";
 
 import { laptop, tablet } from "@/app/screens";
+import Image from "next/image";
 import { styled } from "styled-components";
-
 
 const ContentWrapper = styled.div`
     box-sizing: border-box;
@@ -33,7 +33,7 @@ const ContentWrapper = styled.div`
         height: 90%;
         width: 90%;
     }
-`
+`;
 
 const ContentContainer = styled.div`
     box-sizing: border-box;
@@ -55,10 +55,15 @@ const ContentContainer = styled.div`
         padding: 10px;
         width: 98%;
     }
-`
+`;
 
 const ListItem = styled.div`
-    background: linear-gradient(109.6deg, var(--background-015) 11.2%, var(--background-014) 51.2%, var(--background-005) 98.6%);
+    background: linear-gradient(
+        109.6deg,
+        var(--background-015) 11.2%,
+        var(--background-014) 51.2%,
+        var(--background-005) 98.6%
+    );
     filter: drop-shadow(0 0 1px var(--background-006));
     box-sizing: border-box;
     border-radius: 12px;
@@ -81,7 +86,11 @@ const ListItem = styled.div`
     @media only screen and (min-width: ${tablet}) {
         padding: 20px;
     }
-`
+
+    @media only screen and (min-width: ${laptop}) {
+        width: 45%;
+    }
+`;
 
 const ProjectTitle = styled.h3`
     display: flex;
@@ -90,31 +99,31 @@ const ProjectTitle = styled.h3`
 
     height: auto;
     width: 100%;
-`
+`;
 
 const StrongContainer = styled.strong`
     display: inline;
 
     height: 100%;
     width: auto;
-`
+`;
 
-const Frame = styled.iframe`
+const Frame = styled.div`
     background: white;
     border: none;
     border-radius: 12px;
+    box-sizing: border-box;
 
     display: flex;
     justify-content: center;
     align-items: center;
 
-    height: 450px;
+    position: relative;
+    aspect-ratio: 4/2;
     width: 100%;
 
-    @media only screen and (min-width: ${tablet}) {
-        height: 550px;
-    }
-`
+    overflow: hidden;
+`;
 
 const ButtonWrapper = styled.div`
     display: flex;
@@ -134,10 +143,14 @@ const ButtonWrapper = styled.div`
         margin: 40px 0;
         width: 90%;
     }
-`
+`;
 
 const Button = styled.button`
-    background: linear-gradient(to top, var(--background-007) 0%, var(--background-008) 100%);
+    background: linear-gradient(
+        to top,
+        var(--background-007) 0%,
+        var(--background-008) 100%
+    );
     filter: drop-shadow(0 0 1px var(--background-006));
     box-sizing: border-box;
     border-radius: 12px;
@@ -157,52 +170,104 @@ const Button = styled.button`
 
     &:hover {
         background: linear-gradient(
-            109.6deg, var(--background-015) 11.2%, var(--background-014) 51.2%, var(--background-005) 98.6%
+            109.6deg,
+            var(--background-015) 11.2%,
+            var(--background-014) 51.2%,
+            var(--background-005) 98.6%
         );
     }
 
     @media only screen and (min-width: ${laptop}) {
         padding: 15px;
     }
-`
+`;
 
 const projects = [
-    { name: 'OrionRent', url: 'https://orionrent.vercel.app/', github: 'https://github.com/patrickgunnar/orion-rent-app' },
-    { name: 'Thorpad', url: 'https://thorpad.vercel.app/', github: 'https://github.com/patrickgunnar/thorpad-app' },
-    { name: 'Coralus', url: 'https://coralus.vercel.app/', github: 'https://github.com/patrickgunnar/coralus-app' },
-    { name: 'VirgoChat', url: 'https://virgochat.vercel.app/', github: 'https://github.com/patrickgunnar/virgo-app' },
-    { name: 'Atomico', url: 'https://atomico-todo-app.vercel.app/', github: 'https://github.com/patrickgunnar/atomico-todo-app' },
-    { name: 'SunTime', url: 'https://sun-time-app.vercel.app/', github: 'https://github.com/patrickgunnar/sun-time-app' },
-    { name: 'Spotify Clone', url: 'https://spt-player.vercel.app/', github: 'https://github.com/patrickgunnar/spotify-clone' },
-    { name: 'Airbnb Clone', url: 'https://rbnb-site.vercel.app/', github: 'https://github.com/patrickgunnar/airbnb-clone' }
-]
+    {
+        name: "OrionRent",
+        url: "https://orionrent.vercel.app/",
+        img: "/image/orionrent-app.png",
+        github: "https://github.com/patrickgunnar/orion-rent-app",
+    },
+    {
+        name: "Thorpad",
+        url: "https://thorpad.vercel.app/",
+        img: "/image/thorpad-app.png",
+        github: "https://github.com/patrickgunnar/thorpad-app",
+    },
+    {
+        name: "Coralus",
+        url: "https://coralus.vercel.app/",
+        img: "/image/coralus-app.png",
+        github: "https://github.com/patrickgunnar/coralus-app",
+    },
+    {
+        name: "VirgoChat",
+        url: "https://virgochat.vercel.app/",
+        img: "/image/virgochat-app.png",
+        github: "https://github.com/patrickgunnar/virgo-app",
+    },
+    {
+        name: "Atomico",
+        url: "https://atomico-todo-app.vercel.app/",
+        img: "/image/atomico-app.png",
+        github: "https://github.com/patrickgunnar/atomico-todo-app",
+    },
+    {
+        name: "SunTime",
+        url: "https://sun-time-app.vercel.app/",
+        img: "/image/suntime-app.png",
+        github: "https://github.com/patrickgunnar/sun-time-app",
+    },
+    {
+        name: "Spotify Clone",
+        url: "https://spt-player.vercel.app/",
+        img: "/image/spotify-clone.png",
+        github: "https://github.com/patrickgunnar/spotify-clone",
+    },
+    {
+        name: "Airbnb Clone",
+        url: "https://rbnb-site.vercel.app/",
+        img: "/image/airbnb-clone.png",
+        github: "https://github.com/patrickgunnar/airbnb-clone",
+    },
+];
 
 const ProjectsContent = () => {
     // open in new window handler
     const handleProjectOpen = (url: string) => {
-        window.open(url, '_blank')
-    }
+        window.open(url, "_blank");
+    };
 
     return (
         <ContentWrapper>
             <ContentContainer>
-                {
-                    projects.map(({ name, url, github }) => (
-                        <ListItem key={name}>
-                            <ProjectTitle>
-                                <StrongContainer>{name}</StrongContainer>
-                            </ProjectTitle>
-                            <Frame src={url} title={name} />
-                            <ButtonWrapper>
-                                <Button onClick={() => handleProjectOpen(github)}>Github</Button>
-                                <Button onClick={() => handleProjectOpen(url)}>Live</Button>
-                            </ButtonWrapper>
-                        </ListItem>
-                    ))
-                }
+                {projects.map(async ({ name, url, github, img }) => (
+                    <ListItem key={name}>
+                        <ProjectTitle>
+                            <StrongContainer>{name}</StrongContainer>
+                        </ProjectTitle>
+                        <Frame>
+                            <Image
+                                style={{ objectFit: "cover" }}
+                                src={img}
+                                alt={name}
+                                fill
+                            />
+                        </Frame>
+                        <ButtonWrapper>
+                            <Button onClick={() => handleProjectOpen(github)}>
+                                Github
+                            </Button>
+                            <Button onClick={() => handleProjectOpen(url)}>
+                                Live
+                            </Button>
+                        </ButtonWrapper>
+                    </ListItem>
+                ))}
             </ContentContainer>
         </ContentWrapper>
     );
-}
+};
 
 export default ProjectsContent;
