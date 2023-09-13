c0229f353d29fb39ee9f4390a89fefad7081fd07
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-13 11:31:02 -0300

    feat: added Redux skills

--- src/app/skills/components/SkillsContent.tsx
+++ src/app/skills/components/SkillsContent.tsx
@@ -6,7 +6,7 @@ import { styled } from "styled-components";
 import { 
     SiCss3, SiCypress, SiHtml5, SiJavascript, SiJest, SiMongodb, SiNextdotjs, 
     SiPrisma, SiPusher, SiReact, SiStylelint, SiSupabase, SiTailwindcss, 
-    SiTestinglibrary, SiTypescript 
+    SiTestinglibrary, SiTypescript, SiRedux
 } from "react-icons/si";
 
 
@@ -102,10 +102,11 @@ const StrongContainer = styled.strong`
 
 const skills = [
     { name: 'Next.js', icon: SiNextdotjs }, { name: 'React.js', icon: SiReact }, { name: 'TypeScript', icon: SiTypescript },
-    { name: 'Tailwind', icon: SiTailwindcss }, { name: 'styled-components', icon: SiStylelint }, { name: 'JavaScript', icon: SiJavascript },
-    { name: 'CSS', icon: SiCss3 }, { name: 'HTML5', icon: SiHtml5 }, { name: 'MongoDB', icon: SiMongodb },
-    { name: 'Supabase', icon: SiSupabase }, { name: 'Prisma', icon: SiPrisma }, { name: 'Pusher', icon: SiPusher },
-    { name: 'Jest', icon: SiJest }, { name: 'Testing Library', icon: SiTestinglibrary }, { name: 'Cypress', icon: SiCypress },
+    { name: 'Redux', icon: SiRedux }, { name: 'Tailwind', icon: SiTailwindcss }, { name: 'styled-components', icon: SiStylelint }, 
+    { name: 'JavaScript', icon: SiJavascript }, { name: 'CSS', icon: SiCss3 }, { name: 'HTML5', icon: SiHtml5 }, 
+    { name: 'MongoDB', icon: SiMongodb }, { name: 'Supabase', icon: SiSupabase }, { name: 'Prisma', icon: SiPrisma }, 
+    { name: 'Pusher', icon: SiPusher }, { name: 'Jest', icon: SiJest }, { name: 'Testing Library', icon: SiTestinglibrary }, 
+    { name: 'Cypress', icon: SiCypress },
 ]
 
 const SkillsContent = () => {
