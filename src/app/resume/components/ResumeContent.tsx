9bf39ccd80f379975afda45a486034d265e988ab
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-09-14 20:39:17 -0300

    feat: updated resume

--- src/app/resume/components/ResumeContent.tsx
+++ src/app/resume/components/ResumeContent.tsx
@@ -112,7 +112,7 @@ const ResumeButton = styled.button`
     }
 `
 
-const cvUrl = 'https://drive.google.com/file/d/18KyH_uAtrjq574czNeZPoyNr_tr1KShr/view?usp=sharing'
+const cvUrl = 'https://drive.google.com/file/d/1KO3wOOsdVm0kDsrzie05tJp7IHi0b9jF/view?usp=sharing'
 
 const ResumeContent = () => {
     // cv page open handler
